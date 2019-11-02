243b27d7a64aa8a93b58ff51415635f1d0e46956
fred8728 <fskn@hotmail.com>
2019-11-02 20:25:27 +0100

    done

--- Mandag-onsdag/src/Monday/File1.js
+++
@@ -1,8 +0,0 @@
-
-export const text1 = "Hello";
-export const text2 = "Hello World";
-export const text3 = "Hello Wonderful World";
- 
-export default function(str){
-  return str.toUpperCase();
-}
--- Torsdag/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from "./components/App"
-import apiFacade from "./apiFacade";
-
-ReactDOM.render(<App apiFacade={apiFacade}/>, document.getElementById('root'));
-
-
